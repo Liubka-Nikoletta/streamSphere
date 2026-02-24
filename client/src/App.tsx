9071cf9d7c593f714c867fff790b5fcfaa264f99
Nikoletta <nikolettaliubka@gmail.com>
2026-02-25 00:13:57 +0200

    initialize frontend with Vite, TypeScript and Tailwind v4

---
+++ client/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+      <div className="min-h-screen bg-(--color-dark) flex items-center justify-center">
+          <h1 className="text-2xl leading-tight text-(--color-accent)">Stream Sphere</h1>
+      </div>
+  )
+}
+
+export default App
